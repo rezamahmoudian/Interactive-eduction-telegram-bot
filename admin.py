5ee1536cac7f5b99313133bdb2b7b3febcf844bf
reza <rezamahmoudian80@gmail.com>
2023-02-09 13:12:04 +0330

    add get man and get female students + add create leader cards

--- admin.py
+++ admin.py
@@ -3,8 +3,6 @@ from mysql.connector import errorcode
 import random
 
 
-
-
 # admin_reply_keyboard = [['حذف موضوع', 'افزودن موضوع'],
 #                   ['حذف همه ی موضوعات', 'نمایش موضوعات'],
 #                   ['بازگردانی کارتهای پخش شده', 'پخش کارتها'],
@@ -66,68 +64,89 @@ import random
 #         return CHOOSEACTION
 #
 
-
-def create_cards():
+def get_man_students():
     man = []
+    cnx = mysql.connector.connect(user='root', password='1234', database='telegram_bot')
+    cursor = cnx.cursor()
+
+    queryM = "SELECT * FROM telegram_bot.students WHERE sex = 'M' and login = 1"
+    cursor.execute(queryM)
+    for data in cursor:
+        man.append(data[1])
+    cursor.close()
+    cnx.close()
+    return man
+
+
+def get_female_students():
     female = []
     cnx = mysql.connector.connect(user='root', password='1234', database='telegram_bot')
     cursor = cnx.cursor()
 
-    check = False
-    queryM = "SELECT * FROM telegram_bot.students WHERE sex = 'M' and login = 1"
     queryF = "SELECT * FROM telegram_bot.students WHERE sex = 'F' and login = 1"
-    cursor.execute(queryM)
 
-    for data in cursor:
-        man.append(data[1])
     cursor.execute(queryF)
     for data in cursor:
         female.append(data[1])
-    random.shuffle(man)
-    random.shuffle(female)
-    print(man)
-    print(female)
+
+    cursor.close()
+    cnx.close()
+    return female
 
 
-    subjects = []
+def create_leader_cards(man, female):
+    cnx = mysql.connector.connect(user='root', password='1234', database='telegram_bot')
+    cursor = cnx.cursor()
+
     topics = []
     query_cards = "SELECT * FROM telegram_bot.cards;"
     cursor.execute(query_cards)
     for data in cursor:
         print(data)
         topics.append(data[4])
-        subjects.append(data[0])
 
     topics = list(dict.fromkeys(topics))
-
-    # for key in topics:
-    #     topics[key] = []
-    # cursor.execute(query_cards)
-    # for data in cursor:
-    #     for key in topics:
-    #         print("key "+ key)
-    #         print("data "+ data[4])
-    #
-    #         if data[4] == key:
-    #             topics[key].append(data[2])
-
     print(topics)
-    print(subjects)
     cursor.close()
     cnx.close()
-
-    admin_cards = []
-    if len(man)>len(female):
+    leader_cards = []
+    if len(man) > len(female):
         for i in range(len(topics)):
             card = []
             card.append(topics[i])
             card.append(man[0])
             man.pop(0)
-            admin_cards.append(card)
-
+            leader_cards.append(card)
     print("cards: ")
-    print(admin_cards)
+    print(leader_cards)
     print(man)
+    return leader_cards
+
+
+def create_cards():
+    cnx = mysql.connector.connect(user='root', password='1234', database='telegram_bot')
+    cursor = cnx.cursor()
+
+    man = get_man_students()
+    female = get_female_students()
+
+    random.shuffle(man)
+    random.shuffle(female)
+    print(man)
+    print(female)
+
+    create_leader_cards(man, female)
+
+    subjects = []
+    query_cards = "SELECT * FROM telegram_bot.cards;"
+    cursor.execute(query_cards)
+    for data in cursor:
+        print(data)
+        subjects.append(data[0])
+
+    print(subjects)
+    cursor.close()
+    cnx.close()
 
     cards = []
 
@@ -162,18 +181,18 @@ def create_cards():
     people = man + female
     print(people)
 
-
-
     while len(people) != 0:
         for i in range(len(cards)):
-            cards[i].append(people[0])
-            people.pop(0)
+            if len(people) != 0:
+                cards[i].append(people[0])
+                people.pop(0)
+            else:
+                break
 
     print(people)
     print(cards)
-
+    return cards
 
 
 if __name__ == '__main__':
     create_cards()
-
